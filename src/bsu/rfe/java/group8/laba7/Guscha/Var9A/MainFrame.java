041d5891f97377e4af89548514f86b95681625b7
unknown <usermaksim.gushha@mail.ru>
2021-05-03 12:09:41 +0300

    Add request-handler thread

--- src/bsu/rfe/java/group8/laba7/Guscha/Var9A/MainFrame.java
+++ src/bsu/rfe/java/group8/laba7/Guscha/Var9A/MainFrame.java
@@ -4,11 +4,19 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -109,7 +117,36 @@ public class MainFrame extends JFrame {
                 .addGap(MEDIUM_GAP)
                 .addComponent(messagePanel)
                 .addContainerGap());
-
+// Создание и запуск потока-обработчика запросов
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+                    while (!Thread.interrupted()) {
+                        final Socket socket = serverSocket.accept();
+                        final DataInputStream in = new DataInputStream(socket.getInputStream());
+// Читаем имя отправителя
+                        final String senderName = in.readUTF();
+// Читаем сообщение
+                        final String message = in.readUTF();
+// Закрываем соединение
+                        socket.close();
+// Выделяем IP-адрес
+                        final String address = ((InetSocketAddress) socket.getRemoteSocketAddress())
+                                .getAddress()
+                                .getHostAddress();
+// Выводим сообщение в текстовую область
+                        textAreaIncoming.append(senderName + " (" + address + "): " + message + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    JOptionPane.showMessageDialog(MainFrame.this,
+                            "Ошибка в работе сервера", "Ошибка",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }).start();
     }
 
     public static void main(String[] args) {
